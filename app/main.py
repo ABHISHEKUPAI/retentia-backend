4fb581dcd028d1afe374d6708d5634b665599be8
ABHISHEKUPAI <abhishekupai6055@gmail.com>
2026-05-08 21:17:44 +0530

    fix:shift syllabus endpoint under quiz

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.database import Base, engine
-from app.routers import auth, quiz, syllabus
+from app.routers import auth, quiz
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -15,7 +15,7 @@ Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(quiz.router)
-app.include_router(syllabus.router) 
+
 
 @app.get("/")
 def root():
